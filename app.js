abc9841a9f82b8511dc44cb0bb3edccbe869477f
KUNAL JAIN <kunaljainjain193@gmail.com>
2025-11-21 23:05:57 +0530

    add navbar

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const Listing = require('./models/listing');
 const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+
+
 app.engine('ejs', ejsMate);
 app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const mongoURI = 'mongodb://localhost:27017/wanderlust';
 
